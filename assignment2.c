c4b12fc03cbf7cbba933e73c6abc28a78c0d8a32
Aleksandar Senek <aleks.senek@gmail.com>
2016-02-23 15:00:06 +0100

    TIllagda potentialla peer-funks

--- assignment2.c
+++ assignment2.c
@@ -329,4 +329,148 @@ int compare_doubles (const void *a, const void *b)
     return (*da > *db) - (*da < *db);
 }
 
+void peerpart(double lyst[], int size, int tlevel){
+    
+    int j,i,k;
+
+    for(j = 0; j<tlevel; j++){
+   	 for(i = 0; i<size; i++){
+		if(lyst[i] < j/tlevel){
+		k++;
+		swap(lyst, i, k);
+		}
+   	 } 
+    }
+
+    }
+
+/*
+This i peer to peer main
+ */
+void peerpartQ(double lyst[], int size, int tlevel)
+{
+    int rc, i,j,k;
+    void *status;
+    
+    //Want joinable threads (usually default).
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    for(j = 0; j<tlevel; j++){
+   	 for(i = 0; i<size; i++){
+		if(lyst[i] < j/tlevel){
+		k++;
+		swap(lyst, i, k);
+		}
+   	 } 
+    }
+	
+    
+    //pthread function can take only one argument, so struct.
+    struct thread_data td;
+    td.lyst = lyst;
+    td.low = 0;
+    td.high = size - 1;
+    td.level = tlevel;
+    
+    //The top-level thread.
+    pthread_t theThread;
+    rc = pthread_create(&theThread, &attr, parallelQuicksortHelper,
+                        (void *) &td);
+    if (rc)
+    {
+        printf("ERROR; return code from pthread_create() is %d\n", rc);
+        exit(-1);
+    }
+    
+    //Now join the thread (wait, as joining blocks) and quit.
+    pthread_attr_destroy(&attr);
+    rc = pthread_join(theThread, &status);
+    if (rc)
+    {
+        printf("ERROR; return code from pthread_join() is %d\n", rc);
+        exit(-1);
+    }
+    //printf("Main: completed join with top thread having a status of %ld\n",
+    //		(long)status);
+    
+}
+
+
+/*
+ This is the peer-to-peer helper.
+ */
+void *peerpartQHelp(void *threadarg)
+{
+    int mid, t, rc;
+    void *status;
+    
+    struct thread_data *my_data;
+    my_data = (struct thread_data *) threadarg;
+    
+    //fyi:
+    //printf("Thread responsible for [%d, %d], level %d.\n",
+    //		my_data->low, my_data->high, my_data->level);
+    
+    if (my_data->level <= 0 || my_data->low == my_data->high)
+    {
+        //We have plenty of threads, finish with sequential.
+        quicksortHelper(my_data->lyst, my_data->low, my_data->high);
+        pthread_exit(NULL);
+    }
+    
+    //Want joinable threads (usually default).
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    
+    //Now we partition our part of the lyst.
+    mid = partition(my_data->lyst, my_data->low, my_data->high);
+    
+    //At this point, we will create threads for the
+    //left and right sides.  Must create their data args.
+    struct thread_data thread_data_array[2];
+    
+    for (t = 0; t < 2; t ++)
+    {
+        thread_data_array[t].lyst = my_data->lyst;
+        thread_data_array[t].level = my_data->level - 1;
+    }
+    thread_data_array[0].low = my_data->low;
+    thread_data_array[0].high = mid-1;
+    thread_data_array[1].low = mid+1;
+    thread_data_array[1].high = my_data->high;
+    
+    //Now, instantiate the threads.
+    //In quicksort of course, due to the transitive property,
+    //no elements in the left and right sides of mid will have
+    //to be compared again.
+    pthread_t threads[2];
+    for (t = 0; t < 2; t ++)
+    {
+        rc = pthread_create(&threads[t], &attr, parallelQuicksortHelper,
+                            (void *) &thread_data_array[t]);
+        if (rc)
+        {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+    }
+    
+    pthread_attr_destroy(&attr);
+    //Now, join the left and right sides to finish.
+    for (t = 0; t < 2; t ++)
+    {
+        rc = pthread_join(threads[t], &status);
+        if (rc)
+        {
+            printf("ERROR; return code from pthread_join() is %d\n", rc);
+            exit(-1);
+        }
+    }
+    
+    pthread_exit(NULL);
+}
+
 
